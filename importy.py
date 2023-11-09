b857a810a2296652e0898c34001991d5eb179b27
Lukasz Kolczynski <taktyczny.arab@onet.pl>
2023-11-09 11:11:03 +0100

    Added h1 tag

--- importy.py
+++ importy.py
@@ -1,3 +1,13 @@
 import requests
+# waluta_z
+#waluta_na
+#kwote
+def przelicz(kwota:float, waluta_z:str, waluta_na:str) -> float:
+    url = f"https://v6.exchangerate-api.com/v6/563075f9e11695fd9ef35c70/latest/{waluta_z}"
+    response =requests.get(url)
+    lista_walut = response.json()['conversion_rates']
+    wynik= kwota * lista_walut[waluta_na]
+    return wynik
 
-url = "https://v6.exchangerate-api.com/v6/563075f9e11695fd9ef35c70/latest/USD"
+
+print(przelicz(100,'USD','PLN'))
