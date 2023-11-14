1e4f9b62b00e66999a9e334976d374e24732ee98
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2023-11-14 13:08:02 +0100

    Add

--- daty.py
+++ daty.py
@@ -1,4 +1,19 @@
+                                        # https://strftime.org/
 # Zwrócenie bieżącej daty i godziny:
 from datetime import datetime
-now = datetime.now()
-print(now)
+# now = datetime.now()
+# print(now)
+# print(now.year)
+# print(now.strftime("%Y"))
+
+# # Wyswietl akutalny miesiac jako nazwe, np. "Listopad"
+# print(now.strftime("%b"))
+
+# # Wyswietl akutalny dzien tygodnia jako nazwe, np. "Poniedzialek"
+# print(now.strftime("%A"))
+
+#Konwertuj napis "2023-11-15" na obiekt daty
+date_object = datetime.strptime("2023-11-15", "%Y-%m-%d")
+print(date_object)
+
+past_date = datetime(2023, 11, 15)
