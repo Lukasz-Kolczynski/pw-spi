9fde955f292a730cf7bacf74151a7cbf3b5bf6d0
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2023-11-14 13:23:18 +0100

    Add

--- daty.py
+++ daty.py
@@ -1,7 +1,7 @@
                                         # https://strftime.org/
 # Zwrócenie bieżącej daty i godziny:
 from datetime import datetime
-# now = datetime.now()
+now = datetime.now()
 # print(now)
 # print(now.year)
 # print(now.strftime("%Y"))
@@ -12,8 +12,31 @@ from datetime import datetime
 # # Wyswietl akutalny dzien tygodnia jako nazwe, np. "Poniedzialek"
 # print(now.strftime("%A"))
 
-#Konwertuj napis "2023-11-15" na obiekt daty
-date_object = datetime.strptime("2023-11-15", "%Y-%m-%d")
-print(date_object)
+# #Konwertuj napis "2023-11-15" na obiekt daty
+# date_object = datetime.strptime("2023-11-15", "%Y-%m-%d")
+# print(date_object)
 
-past_date = datetime(2023, 11, 15)
+# past_date = datetime(2023, 11, 15)
+
+# # 2023-Nov-15
+# from datetime import timedelta
+# # Dodaj 5 dni do aktualnej daty
+# print(now + timedelta(days=5))
+# # Odejmij 2 tygodnie od aktualnej daty
+# print(now - timedelta(weeks=2))
+
+# #Wyswietl roznice w dniach miedzy 1 stycznia 2023 a dzisiaj
+# past_date = datetime(2023, 1, 1)
+# day= now - past_date
+# print(day.days)
+
+# import calendar
+# #Sprawdz czy rok 2024 jest rokiem przestepnym
+# rok = 2024
+# if calendar.isleap(rok):
+#     print(f'{rok} jest rokiem przestepnym')
+# else:
+#     print(f'{rok} nie jest rokiem przestepnym')
+
+# wyswietl numer biezacego roku tygodnia
+print(now.strftime("%U"))
