ddb365077fd67d30031a494e733cd7d472459506
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2023-11-16 11:48:58 +0100

    add

--- pw-spi/regex.py
+++ pw-spi/regex.py
@@ -1,9 +1,9 @@
 import re
 
-txt= "Dopasowuje pozycje, która nie jest granicą słowa."
+# txt= "Dopasowuje pozycje, która nie jest granicą słowa."
 
-x= re.search("^Dopasowuje.*słowa.$", txt)
-print(x)
+# x= re.search("^Dopasowuje.*słowa.$", txt)
+# print(x)
 
 # findall - zwracaja liste zawierajaca wszystkie wystapienia
 # search - zwraca obiekt match, jesli w dowolnym miejscu znajdzie dopasowanie
@@ -22,5 +22,41 @@ print(x)
 
 
 txt= "Dopasowuje nie poznieycje, która nie jest grannieicą nie słowa."
-x=re.findall("nie",txt)
+# x=re.findall("nie",txt,)
+# print(x)
+
+# x=re.split("\s",txt,)
+# print(x)
+
+# x=re.sub("nie","WOW",txt)
+# print(x)
+
+# x= re.findall(r'\bnie\b',txt)
+# print(x)
+
+# x= re.findall(r'[\w]+',txt)
+# print(x)
+
+# mail="jakub.chmielak@pw.edu.pl"
+# x=re.match("^[\w\.]+@[\w\.]+",mail)
+# print(bool(x))
+# # x=re.split("@",mail)
+# # print(x)
+
+# txt1="rok 2023 bedzie lepszy."
+# x=re.sub("\d",'X',txt)
+# print(x)
+
+# x=re.findall(r"\b[n]\w+", txt)
+# print(x)
+
+# kot="Nasz kot ma 6 lat i waży 4kg."
+# x=re.findall("\d+",kot)
+# print(x)
+
+# x=re.search(r"^nasz",kot, re.IGNORECASE)
+# print(x)
+
+text= "Mój numer to 1230-456-7890."
+x=re.search(r"\d{3}+-\d{3}+-\d{3}+",text)
 print(x)
