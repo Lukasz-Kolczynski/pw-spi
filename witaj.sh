62b8c28e1b6039f066a3b869260b03222906834e
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2023-11-22 16:12:33 +0100

    [add]

--- witaj.sh
+++ witaj.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "Witaj świecie"
+# echo "Witaj świecie"
+
+# nazwa=Jan
+
+# echo "Witaj, $nazwa"
+
+
+a=10
+
+b=10
+
+if [ $a -eq $b]: then
+    echo "Równe"
+else
+    echo "Różne"
+fi
