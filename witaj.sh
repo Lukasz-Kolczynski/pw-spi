7255bda4f9ee73c35b73d6afb4b77c64c68928d9
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2023-12-14 09:31:43 +0100

    ADD

--- witaj.sh
+++ witaj.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-
+#!/bin/bash 
+# ./witaj.sh
 # echo "Witaj świecie"
 
 # nazwa=Jan
@@ -7,12 +7,37 @@
 # echo "Witaj, $nazwa"
 
 
-a=10
+# a=10
 
-b=10
+# b=10
 
-if [ $a -eq $b]: then
-    echo "Równe"
+# if [ $a -eq $b]: then
+#     echo "Równe"
+# else
+#     echo "Różne"
+# fi
+
+
+# echo "Jak masz na imie?"
+# read imie
+# echo "Cześć $imie!"
+
+
+echo "Podaj liczbe"
+read liczba
+if [ $liczba -gt 10 ]
+then
+    echo "Liczba większa od 10"
 else
-    echo "Różne"
+    echo "Liczba mniejsza lub rowna 10"
 fi
+
+for i in {1.liczba}
+do
+    echo " Iteracja $i"
+done
+
+while [ $liczba -le 5 ]
+do  echo "Liczba: $liczba"
+    ((liczba++))
+
