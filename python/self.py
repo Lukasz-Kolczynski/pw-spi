c23718b509b49573540c035e0bfb34044b5512b0
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2024-04-09 09:03:18 +0200

    [selfv2]

--- python/self.py
+++ python/self.py
@@ -1,7 +1,9 @@
 class Node:
-    __init__(self,data):
+   def __init__(self,data) -> None:
     self.data = data  #Przechowywanie danych elementu
     self.next = None #Wskaźnik na następny element, początkowe None
+    self.prev = None
+
 
 class SinglyLinkedList:
     def __init__(self):
@@ -15,5 +17,32 @@ class SinglyLinkedList:
         last = self.head
         while last.next:
             last = last.next #Przechodzenie do ostatniego elementu
+            
         last.next = Node(data) #Tworzenie nowego elementu na koncu
-        
+
+
+    def display_all(self):
+        current = self.head
+        while current:
+            print(current.data, " ")
+            current = current.next
+
+
+    def display_all_(self,data):
+        current= self.head
+        while current:
+            next_value = current.next.data if current.next else "None"
+            print(f"Value: {current.data} , Next {current.next}")
+            return
+        current = current.next
+        #Wyswietlenie wszystkich wartosci wraz z nastepna wartoscia
+        return 0
+    
+
+lista = SinglyLinkedList()
+lista.append(1)
+lista.append(2)
+lista.append(3)
+
+lista.display_all()
+
