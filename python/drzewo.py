0bced7d2f436ea68046b2a3b5d4418965b0d77d0
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2024-04-16 11:12:38 +0200

    [przeszukiwanie_w_głąbDFS]

--- python/drzewo.py
+++ python/drzewo.py
@@ -1,21 +1,46 @@
-tree = [1,2,3,4,5,6,7]
+# tree = [1,2,3,4,5,6,7]
 
-def get_left_child(index):
-    """Zwraca lewe dziecko węzła o danym indeksie."""
-    return 2 * index + 1
+# def get_left_child(index):
+#     """Zwraca lewe dziecko węzła o danym indeksie."""
+#     return 2 * index + 1
 
-def get_right_child(index):
-    """Zwraca prawe dziecko węzła o danym indeksie."""
-    return 2 * index + 2
+# def get_right_child(index):
+#     """Zwraca prawe dziecko węzła o danym indeksie."""
+#     return 2 * index + 2
 
-def get_parent(index):
-    """Zwraca rodzica węzła o danym indeksie."""
-    return (index-1) // 2
+# def get_parent(index):
+#     """Zwraca rodzica węzła o danym indeksie."""
+#     return (index-1) // 2
 
-index= 0
-left_child_index = get_left_child(index)
-right_child_index = get_right_child(index)
-parent_index = get_parent(index)
-print(f"Lewe:  {left_child_index}")
-print(f"Prawe:  {right_child_index}")
-print(f"Rodzic:  {parent_index}")
+# index= 0
+# left_child_index = get_left_child(index)
+# right_child_index = get_right_child(index)
+# parent_index = get_parent(index)
+# print(f"Lewe:  {left_child_index}")
+# print(f"Prawe:  {right_child_index}")
+# print(f"Rodzic:  {parent_index}")
+
+
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None 
+
+def dfs_preorder(node):
+    if node:
+        print(node.value)
+        dfs_preorder(node.left)
+        dfs_preorder(node.right)
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Przeszukiwanie w głąb(DFS) - Pre-order:")
+dfs_preorder(root)
