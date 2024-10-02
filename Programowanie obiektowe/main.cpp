28904215a189dcec7330a27c2780ec40bba95839
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2024-10-02 16:14:22 +0200

    [main.cpp]

---
+++ Programowanie obiektowe/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Product
+{
+    Product(const char *name, unsigned int price)
+    {
+        std::cout << "Konstruktor: Allokujemy pamięć\n";
+        name = new char[strlen(name)];
+        sprintf(this-> name, "%s", name));
+
+    }
+
+    ~Product()
+    {
+        std::cout << "Destruktor: Zwalniamy pamięć\n"
+    }
+
+    private:
+        char *name;
+};
+
+int main()
+{
+    for(int i=0; i< 100000000; i++)
+    {
+        Product laptop("Dell", 1234);
+    }
+    while(1);
+    std::cout << "Test" << std::endl
+}
