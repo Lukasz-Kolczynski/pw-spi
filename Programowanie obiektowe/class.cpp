a0a4414f44c37308661884b26730c81cefe93d34
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2024-10-09 17:07:33 +0200

    [class_update]

--- Programowanie obiektowe/class.cpp
+++ Programowanie obiektowe/class.cpp
@@ -2,13 +2,28 @@ class Complex {
     public:
         Complex(double _re, double _im);
         Complex(double _re);
-        double real();
-        double imag();
+        double real() const;
+        double imag() const;
+        Complex operator+(const Complex &c); // Complex + Complex
+        Complex operator-(const Complex &c); // Complex - Complex
+        Complex& operator+=(const Complex &c); // Complex += Complex
+        Complex& operator-=(const Complex &c); // Complex -= Complex
+        Complex operator*(double x); // Complex * double
+        Complex operator-(); // operator unarny (jednoargumentowy)
 
 
+    a + b
+    a - b 
+    a += b 
+    a -= b 
+    -a
+    a (Complex) * b (double)
+    std::ostream <<
 
     private:
         double re;
         double im;
 
 };
+
+std::ostream& operator<<(std::ostream &os, const Complex &obj);
