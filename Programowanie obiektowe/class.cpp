d858bf0103505faf6434a1954c6181c84b7bf7ce
�ukasz Piotr Kolczy�ski <335775@DC=sntis,DC=local>
2024-10-09 16:51:23 +0200

    [class_c++]

---
+++ Programowanie obiektowe/class.cpp
@@ -0,0 +1,14 @@
+class Complex {
+    public:
+        Complex(double _re, double _im);
+        Complex(double _re);
+        double real();
+        double imag();
+
+
+
+    private:
+        double re;
+        double im;
+
+};
