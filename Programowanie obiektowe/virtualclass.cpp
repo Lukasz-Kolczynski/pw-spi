54a9506fbe3d150b590f7a8c64384b75cc5a5e83
Łukasz Kolczyński <taktyczny.arab@onet.pl>
2024-11-06 17:42:17 +0100

    [Diamond_Problem]

--- Programowanie obiektowe/virtualclass.cpp
+++ Programowanie obiektowe/virtualclass.cpp
@@ -3,7 +3,7 @@
 
 class Car {
 public:
-    void drive() {std::cout << " brum brum\n"; };
+    void drive() {std::cout << "Drive " << vin << std::endl; };
     void setVin(const std::string &_vin) { this ->vin = _vin; }
 private:
     std::string manufacturer;
@@ -18,7 +18,7 @@ public:
 
 };
 
-class ElectricCar :  public Car {
+class ElectricCar :  virtual public Car {
 public:
     int getBatteryCapacity();
 };
@@ -26,3 +26,17 @@ public:
 class HybridCar : public PetrolCar, public ElectricCar {
 
 };
+
+int main() {
+    HybridCar hc;
+    PetrolCar &pc = static_cast<PetrolCar&>(hc);
+    ElectricCar &ec = static_cast<ElectricCar&>(hc);
+
+    pc.setVin("40285278352543");
+    ec.setVin("45345423");
+
+    pc.drive();
+    ec.drive();
+
+    return 0;
+}
