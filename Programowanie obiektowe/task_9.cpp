a001302524976d3d3e2da327df305dcd97b16100
Łukasz Kolczyński <taktyczny.arab@onet.pl>
2024-11-27 19:47:48 +0100

    [occurences]

--- Programowanie obiektowe/task_9.cpp
+++ Programowanie obiektowe/task_9.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 
 
+
 template<typename T>
 class Container 
 {
@@ -11,7 +12,72 @@ public:
     virtual bool isEmpty() const=0;
     virtual size_t size() const=0;
 
-    ~Container() {}
+    ~Container() 
+    {
+        while (!isEmpty())
+        {
+            pop();
+        }
+        
+    }
+
+
+};
+
+
+
+template <typename T>
+class Node {
+public:
+    Node(T value) : data(value), next(nullptr) {}
+    T data;
+    Node<T> *next;
+};
+
+
+
+template <typename T>
+class Queue : public Container<T>
+{
+private:
+    Node<T> *head;
+    Node<T> *tail;
+    size_t  count;
+
+public:
+    Queue() : head(nullptr), tail(nullptr), count(0)
+    {
+
+    }
+    
+    void push(const T &value) override
+    {
+        Node<T> *_Node =new Node<T>(value);
+        if(isEmpty())
+        {
+            head = tail =_Node;
+        }
+        else
+        {
+            tail->next = _Node;
+            tail = _Node;
+        }
+
+        ++count;
+    }
+
+    T pop()
+    {
+        if (isEmpty())
+        {
+            throw std::out_of_range("Queue is empty");
+        }
+    }
+
+    bool isEmpty() override
+    {
+        return false;
+    }
 
 };
 
