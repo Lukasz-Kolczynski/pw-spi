4ec7a0c0c4bd86306465d60a3934a27c896c86b6
Łukasz Kolczyński <taktyczny.arab@onet.pl>
2024-12-10 15:49:15 +0100

    [completed_program]

--- C-language/concurrent .c
+++ C-language/concurrent .c
@@ -6,18 +6,16 @@
 
 char *buffer = NULL;
 char *where_file = NULL;
-int running_flag =1;
+int running_flag = 1;
 
-void load_file() 
-{
+void load_file() {
     if (!where_file) return;
 
     FILE *file = fopen(where_file, "r");
-    if (!file)
-        {
-            print("Unable to open file:%s\n.",where_file);
-            return;
-        }
+    if (!file) {
+        printf("Unable to open file: %s\n", where_file);
+        return;
+    }
 
     fseek(file, 0, SEEK_END);
     size_t size = ftell(file);
@@ -25,51 +23,44 @@ void load_file()
 
     free(buffer);
     buffer = malloc(size + 1);
-    if (buffer)
-        {
-            fread (buffer, 1, size, file);
-            buffer[size] = "\0";
-            printf("File was loaded.\n");
-        } else {
-            printf("Error in allocation.\n");
-        }
+    if (buffer) {
+        fread(buffer, 1, size, file);
+        buffer[size] = '\0';
+        printf("File was loaded.\n");
+    } else {
+        printf("Error in allocation.\n");
+    }
 
-        fclose(file);
-
+    fclose(file);
 }
 
-void clear_buffer()
-{
+void clear_buffer() {
     free(buffer);
     buffer = NULL;
-    printf ("Buffer has cleared.\n");
+    printf("Buffer has been cleared.\n");
 }
 
-void handler1(int signo)
-{
-    printf("\nSIGUSR1 Caught signal1 %d (%s)\n"); 
-	(void)signo;
-	load_file();
+void handler1(int signo) {
+    printf("\nSIGUSR1 caught signal %d (%s)\n", signo, strsignal(signo));
+    load_file();
 }
 
-void handler2(int signo) 
-{
-    printf("\nSIGUSR2 Caught signal2 %d (%s)\n");
-    (void)signo;
+void handler2(int signo) {
+    printf("\nSIGUSR2 caught signal %d (%s)\n", signo, strsignal(signo));
     clear_buffer();
 }
 
 void handler3(int signo) {
-    (void)signo;
     printf("\nSIGINT received (CTRL+C). Do you want to exit? (y/n): ");
+    fflush(stdout);
     char response = getchar();
+    getchar();
     if (response == 'y' || response == 'Y') {
         running_flag = 0;
     } else {
         printf("Continuing program...\n");
     }
 }
-
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -79,9 +70,9 @@ int main(int argc, char *argv[]) {
 
     where_file = argv[1];
 
-    signal(SIGUSR1, handler1); 
-    signal(SIGUSR2, handler2); 
-    signal(SIGINT, handler3); 
+    signal(SIGUSR1, handler1);
+    signal(SIGUSR2, handler2);
+    signal(SIGINT, handler3);
 
     load_file();
 
