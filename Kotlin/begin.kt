e596fa6d003b76701db7acf566be34def5ba729e
Łukasz Kolczyński <taktyczny.arab@onet.pl>
2025-02-21 17:39:35 +0100

    [Kotlin_begining]

--- Kotlin/begin.kt
+++ Kotlin/begin.kt
@@ -1,4 +1,47 @@
-fun main()
+fun main() {
+    println("Hello world!")
+    strFunc()
+    mathFunc()
+}
+
+//val - immutable
+//var - mutable 
+
+fun mathFunc() 
 {
-    println("Witaj w Kotlinie!")
+    val x = 7
+    val y = 3 
+    println(x+y)
+    print("Sum in other way")
+    val sum = x.plus(y)
+    println("Result: $sum")
+
+    val numOfSudents = 50
+    if (numOfSudents in 1..50)
+    {
+        println(numOfSudents)
+    }else
+    {
+        print("Inacessable")
+    }
+
+    val res  = 53
+    when(res)
+    {
+        4 -> print("First case")
+        in 1..40 ->print("In range")
+        else->print("Invaliid res")
+    }
+
+
+
+}
+
+
+fun strFunc()
+{
+    val myStr = "Johnny"
+    val secondStr : String
+    secondStr = "Kowalski"
+    println("My name is $myStr $secondStr")
 }
