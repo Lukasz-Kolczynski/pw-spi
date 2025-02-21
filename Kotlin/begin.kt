ee1f09155527416765c6f240e77a6c879b95a4f7
Łukasz Kolczyński <taktyczny.arab@onet.pl>
2025-02-21 17:49:22 +0100

    [update_kotlin]

--- Kotlin/begin.kt
+++ Kotlin/begin.kt
@@ -2,46 +2,45 @@ fun main() {
     println("Hello world!")
     strFunc()
     mathFunc()
+    repeatFunc()
 }
 
-//val - immutable
-//var - mutable 
+// val - immutable
+// var - mutable
 
-fun mathFunc() 
-{
+fun mathFunc() {
     val x = 7
-    val y = 3 
-    println(x+y)
-    print("Sum in other way")
+    val y = 3
+    println(x + y)
+    print("Sum in other way: ")
     val sum = x.plus(y)
     println("Result: $sum")
 
-    val numOfSudents = 50
-    if (numOfSudents in 1..50)
-    {
-        println(numOfSudents)
-    }else
-    {
-        print("Inacessable")
+    val numOfStudents = 50
+    if (numOfStudents in 1..50) {
+        println(numOfStudents)
+    } else {
+        println("Inaccessible")
     }
 
-    val res  = 53
-    when(res)
-    {
-        4 -> print("First case")
-        in 1..40 ->print("In range")
-        else->print("Invaliid res")
+    val res = 53
+    when (res) {
+        4 -> println("First case")
+        in 1..40 -> println("In range")
+        else -> println("Invalid res")
     }
-
-
-
 }
 
-
-fun strFunc()
-{
+fun strFunc() {
     val myStr = "Johnny"
-    val secondStr : String
+    val secondStr: String
     secondStr = "Kowalski"
     println("My name is $myStr $secondStr")
+}
+
+fun repeatFunc() {
+    repeat(2)
+    {
+        println("Hello")
+    }
 }
