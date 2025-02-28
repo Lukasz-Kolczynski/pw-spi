c2636df809d1d0c5bce8aff82f959df6aef44b56
Łukasz Kolczyński <taktyczny.arab@onet.pl>
2025-02-28 13:57:21 +0100

    [next_task]

--- python/bib_pandas.py
+++ python/bib_pandas.py
@@ -10,3 +10,13 @@ def load_file(file_path):
         except:
             df = pd.read_csv(file_path, sep = '\t', header = None)
     return df
+
+
+cols = ["timestamp", "id", "dc1", "dc2", "dyskretne", "names", "wczwo1", "wczwo2", "wczwo_plus", "wczwo_minus", "x", "y", "z"]
+
+
+
+df = load_file('/home/u335775/Pulpit/Łukasz Kolczyński/pw-spi/python/data_01.dat')
+print(df)
+df.columns = cols
+print(df[['timestamp', 'id']]) 
