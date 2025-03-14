1a9aa0ffa561a771398be15f3ea26d8da139643b
Łukasz Kolczyński <taktyczny.arab@onet.pl>
2025-03-14 14:46:55 +0100

    [sorting_number_in_files]

--- python/file_sorting.py
+++ python/file_sorting.py
@@ -99,11 +99,47 @@ def sort_data_in_directory(working_directory):
                 result_file.write(str(data[i])+ "\n")
             result_file.write(str(data[-1]))
 
+
+def merge_two_file(working_directory, file_in_1_name, file_in_2_name, file_out_name):
+    file_in_1_path = os.path.join(working_directory, file_in_1_name)
+    file_in_2_path = os.path.join(working_directory, file_in_2_name)
+    file_out_path = os.path.join(working_directory, file_out_name)
+
+    with open(file_in_1_path, "r") as file_in_1:
+        with open(file_in_2_path, "r") as file_in_2:
+            with open(file_out_path, "w") as file_out:
+                line_1 = file_in_1.readline().strip()
+                line_2 = file_in_2.readline().strip()
+
+                while True:
+                    if line_1 and line_2:
+                        v1 = int(line_1)
+                        v2 = int(line_2)
+
+                        if v1 < v2:
+                            file_out.write(str(v1))
+                            line_1 = file_in_1.readline().strip()
+                        else:
+                            file_out.write(str(v2))
+                            line_2 = file_in_2.readline().strip()
+                    elif line_1 and not line_2:
+                        file_out.write(line_1)
+                        line_1 = file_in_1.readline().strip()
+                    elif not line_1 and line_2:
+                        file_out.write(line_2)
+                        line_2 = file_in_2.readline().strip()
+                    else:
+                        break
+
+                    file_out.write("\n")
+
+
 def main():
     begin = timer()
     ###generate_data("data.dat", 10, 20) #generuje plik z 10-cioma randomowymi liczbami w zakresie 0-20
     ##divide_file("data.dat", 4, "/home/u335775/Pulpit/Łukasz Kolczyński/pw-spi/python/data_file")   #dzieli wygenerowany plik na mniejsze pliki po 4 liczby w każdym
     #sort_data_in_directory("/home/u335775/Pulpit/Łukasz Kolczyński/pw-spi/python/data_file")  #sortuje liczby rosnąco w każdym pliku
+    merge_two_file("/home/u335775/Pulpit/Łukasz Kolczyński/pw-spi/python/data_file", "data_1.dat", "data_2.dat", "data_1_2.dat")
     end = timer()
     print(f"Time: (end - begin) s")
 
